37ea912b732734823f95e33ab066839cce33e0ff
Roger Santos <roger.santos.main@gmail.com>
2017-08-19 20:31:05 -0500

    Added example of Sync and Async calls

--- node_js/native_abstraction/source/main.js
+++ node_js/native_abstraction/source/main.js
@@ -3,4 +3,15 @@ const nativeAddon = require('../stage/node_modules/native_addon');
 
 // `Hello` function returns a string, so we have to console.log it!
 console.log(nativeAddon.Run());
-console.log(nativeAddon.Run("String from JavaScript!"));
+console.log(nativeAddon.Run("1 - SyncCall"));
+
+nativeAddon.RunAsync("1 - AsyncCall", function(err, result) {
+    console.log(result);
+});
+
+nativeAddon.RunAsync("2 - AsyncCall", function(err, result) {
+    console.log(result);
+});
+
+console.log(nativeAddon.Run("2 - SyncCall"));
+console.log(nativeAddon.Run("3 - SyncCall"));
